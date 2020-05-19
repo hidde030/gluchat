910c59bd42fe7c0a481329ffedfff13c9e7c4f40
Hidde030 <530643@student.glu.nl>
2020-05-19 13:55:00 +0200

    heroku link

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const app = require('express')();
 const server = require('http').Server(app);
-const io = require('socket.io') (server);
-const port = 3000;
+const io = require('socket.io')(server);
+const port = process.env || 3000;
 
-server.listen(port,() =>{
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
-})
+});
 
-io.on('connection', (socket) => {
-    console.log('user connected');
-    socket.emit('message', {hidde: 'hey hoe gaat het?'});
-    socket.on('another event', (data)=>{
-        console.log(data);
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html');
+});
+
+app.get('/swift', (req, res) => {
+    res.sendFile(__dirname + '/public/swift.html');
+});
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/css.html');
+});
+
+// tech namespace
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New user joined ${data.room} room!`);
+    })
+
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+
+        tech.emit('message', 'user disconnected');
     })
 })
